720455fbc454c10b7a8eb514893eb5ddfeec0b55
Pavel <iampetrovpavel@gmail.com>
2021-12-27 22:54:35 +0300

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -4,7 +4,7 @@ import {CustomError} from "../errors/custom-error";
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 
     if(err instanceof  CustomError){
-        res.status(err.statusCode).send({errors: err.serializeErrors()})
+        return res.status(err.statusCode).send({errors: err.serializeErrors()})
     }
 
     console.error(err);
