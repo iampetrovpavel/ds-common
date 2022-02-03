96e58e3c813fecd7bf4f21c1777b18064cd846b4
Pavel <iampetrovpavel@gmail.com>
2022-02-03 20:27:50 +0300

    Updates

--- src/middlewares/is-admin.ts
+++ src/middlewares/is-admin.ts
@@ -2,6 +2,7 @@ import {NextFunction, Request, Response} from "express"
 import {NotAuthorizedError} from "../errors/not-authorized-error"
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+    console.log("User: ", req.currentUser)
     if(!req.currentUser || req.currentUser.groups.indexOf('admin')===-1){
         throw new NotAuthorizedError()
     }
